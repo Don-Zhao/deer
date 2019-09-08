d6a48a338d15eed9128a83a70f88c3c8856373ae
admin <admin@163.com>
2019-09-08 22:21:42 +0800

    security completed!

---
+++ src/main/java/com/deer/app/config/DeerWebSecurityAdapter.java
@@ -0,0 +1,20 @@
+package com.deer.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class DeerWebSecurityAdapter extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/css/**", "/js/**", "/images/**", "/common.html").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin().loginPage("/login").permitAll();
+	}
+}
