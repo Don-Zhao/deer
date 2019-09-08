53a853e61a6f7049bfd5016f1aa0da12af2f27b7
admin <admin@163.com>
2019-09-08 20:02:43 +0800

    添加spring security

---
+++ src/main/java/com/deer/app/controller/NoFolderController.java
@@ -0,0 +1,13 @@
+package com.deer.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class NoFolderController {
+	
+	@RequestMapping("/home")
+	public String test() {
+		return "home";
+	}
+}
