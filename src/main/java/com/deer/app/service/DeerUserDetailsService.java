dab27e58171aff47408ed92685824fc3377ef7e9
admin <admin@163.com>
2019-09-08 22:47:05 +0800

    UserDetailsService

---
+++ src/main/java/com/deer/app/service/DeerUserDetailsService.java
@@ -0,0 +1,14 @@
+package com.deer.app.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class DeerUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return null;
+	}
+
+}
